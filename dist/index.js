08e1137c3b31bfd58ec5246b508a6375f938e4b8
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-12-28 16:17:59 +0800

    changed for typescript style

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+;(function () {
+  define(['./LargeLocalStorage'], function (lls) {
+    console.log('lls', lls);
+    // var storage = new lls({size: 100 * 1024 * 1024});
+    if (typeof exports === 'object') {
+      module.exports = lls;
+    } else if (typeof define === 'function' && define.amd) {
+      define(function () {
+        return lls;
+      });
+    } else {
+      this.lls = lls;
+    }
+  });
+}).call(this);
